6419a3290bff0003bd5f020862e33d635ecea108
emmacintosh <emmammrussell@gmail.com>
2022-05-16 16:04:25 -0600

    add post-9pm work on library

--- src/Library.js
+++ src/Library.js
@@ -28,7 +28,10 @@ function createLibrary(libby){
 
 //only one parameter needed
 
-
+//yeah, yknow what, i'm over this whole "goofy variable
+//names to remind myself the keys and items and
+//parameters are different things" deal, gonna
+//use normal variables now
 
 function addBook(library, book) {
 return library.shelves[book.genre].push(book);
@@ -51,11 +54,34 @@ return library.shelves[book.genre].push(book);
 
 function checkoutBook(library, book, genre) {
 for (var i = 0; i < genre.length; i++) {
-  if (book.genre === book)
+  if(library.shelves[genre][i].title === book) {
+library.shelves[genre].splice([i], 1);
+  return `You have now checked out ${book} from the ${library.name}`
+} else {
+//library.shelves[genre].splice([i], 1);
 
-} return
+    }
+  }
+    return `Sorry, there are currently no copies of ${book} available at the ${library.name}`
 }
 
+//g.i.o.s.
+//goal
+//inputs to functions
+//outputs
+//setup
+
+//dot notation finds properties, typing genre with
+//dot notation would look for the key genre which is not
+//there
+
+//genre evaluates to a string
+
+//no dot needed between shelves and genre
+//genre in bracket notation because it's referring
+//to an array
+// go to the library, go to the shelves, iterate
+// over genre, then title
 
 //if library.shelves[genre].includes(title) {
 //  return library.shelves[book.genre].(book);
